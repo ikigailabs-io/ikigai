57db2dc1b282d9cba1d7a9231304ee1cbe88c0d5
Harsh Parekh <harsh@ikigailabs.io>
2024-12-09 23:46:20 -0500

    Fix import for Self

--- src/ikigai/components/project.py
+++ src/ikigai/components/project.py
@@ -3,10 +3,16 @@
 # SPDX-License-Identifier: MIT
 
 from __future__ import annotations
+import sys
 from datetime import datetime
-from typing import Self
 from pydantic import BaseModel, EmailStr
 from ikigai.client.session import Session
+
+# Multiple python version compatible import for Self
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
 
 
 class Project(BaseModel):
