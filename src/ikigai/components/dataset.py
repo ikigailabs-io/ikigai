ae9e792f0ec3b7b53e494c98a6913440f419c66b
Aayush <194560153+aayush-ikigailabs@users.noreply.github.com>
2025-01-14 01:32:29 +0530

    Bugfix: dataset multi part upload (#10)
    * use correct headers for complete-dataset-multipart-upload
    
    * lint checks
    
    * simplify and always use the multipart upload for dataset upload

--- src/ikigai/components/dataset.py
+++ src/ikigai/components/dataset.py
@@ -24,29 +24,7 @@ from ikigai.utils.protocols import Directory
 CHUNK_SIZE = int(50e6)  # 50 MB
 
 
-def __single_part_upload_data(
-    session: Session, app_id: str, dataset_id: str, data: bytes, filename: str
-) -> None:
-    resp = session.get(
-        path="/component/get-dataset-upload-url",
-        params={
-            "dataset_id": dataset_id,
-            "project_id": app_id,
-            "filename": filename,
-        },
-    ).json()
-
-    upload_url = resp["url"]
-    content_type = resp["content_type"]
-    with requests.session() as request:
-        request.headers.update(
-            {"Content-Type": content_type, "Cache-Control": "no-cache"}
-        )
-        resp = request.put(url=upload_url, data=data)
-        assert resp.status_code == HTTPStatus.OK
-
-
-def __multi_part_upload_data(
+def __upload_data(
     session: Session,
     app_id: str,
     dataset_id: str,
@@ -71,6 +49,7 @@ def __multi_part_upload_data(
         int(chunk_idx): upload_url for chunk_idx, upload_url in resp["urls"].items()
     }
     upload_id = resp["upload_id"]
+    etags: dict[int, str] = {}
 
     try:
         with requests.session() as request:
@@ -79,11 +58,16 @@ def __multi_part_upload_data(
             )
             for chunk_idx, upload_url in upload_urls.items():
                 chunk_start, chunk_end = (
+                    (chunk_idx - 1) * chunk_size,
                     chunk_idx * chunk_size,
-                    (chunk_idx + 1) * chunk_size,
                 )
                 chunk = data[chunk_start:chunk_end]
                 resp = request.put(url=upload_url, data=chunk)
+                assert resp.status_code == HTTPStatus.OK
+
+                # Get etags from response header
+                etags[chunk_idx] = resp.headers.get("ETag")
+
     except Exception:
         session.post(
             path="/component/complete-dataset-multipart-upload",
@@ -110,7 +94,7 @@ def __multi_part_upload_data(
             },
             "abort": False,
             "upload_id": upload_id,
-            "etags": upload_urls,
+            "etags": etags,
         },
     )
 
@@ -119,31 +103,16 @@ def _upload_data(
     session: Session, app_id: str, dataset_id: str, name: str, data: bytes
 ) -> None:
     assert data is not None
-    size = len(data)
     filename = f"{name}.csv"
 
-    """
-    Seperate logic for multi/single part upload
-    https://ikigailabs.atlassian.net/browse/IPLT-7277
-    TODO: Simplify once the above ticket is addressed
-    """
-    if size > CHUNK_SIZE:
-        __multi_part_upload_data(
-            session=session,
-            app_id=app_id,
-            dataset_id=dataset_id,
-            data=data,
-            filename=filename,
-            chunk_size=CHUNK_SIZE,
-        )
-    else:
-        __single_part_upload_data(
-            session=session,
-            app_id=app_id,
-            dataset_id=dataset_id,
-            data=data,
-            filename=filename,
-        )
+    __upload_data(
+        session=session,
+        app_id=app_id,
+        dataset_id=dataset_id,
+        data=data,
+        filename=filename,
+        chunk_size=CHUNK_SIZE,
+    )
 
     upload_completion_time = time.time()
     session.get(
