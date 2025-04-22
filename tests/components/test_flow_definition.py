33798a4492c240c1dbb4cc7fd110d963e5840f8d
Harsh Parekh <harsh@ikigailabs.io>
2025-04-22 14:42:36 -0400

    Add FacetSpecs parsing

--- tests/components/test_flow_definition.py
+++ tests/components/test_flow_definition.py
@@ -8,6 +8,28 @@ from contextlib import ExitStack
 from ikigai import Ikigai
 
 
+def test_flow_definition_builder_facet_types(
+    ikigai: Ikigai,
+) -> None:
+    facet_types = ikigai.builder.facet_types
+    assert facet_types.INPUT
+    assert facet_types.MID
+    assert facet_types.OUTPUT
+
+    # Assorted facet types tests
+    assert "PYTHON" in facet_types.INPUT
+    assert "PYTHON" in facet_types.MID
+    assert "PYTHON" in facet_types.OUTPUT
+
+    assert "IMPORTED" in facet_types.INPUT
+    assert "IMPORTED" not in facet_types.MID
+    assert "IMPORTED" not in facet_types.OUTPUT
+
+    assert "EXPORTED" not in facet_types.INPUT
+    assert "EXPORTED" not in facet_types.MID
+    assert "EXPORTED" in facet_types.OUTPUT
+
+
 def test_flow_definition_empty(
     ikigai: Ikigai,
     app_name: str,
