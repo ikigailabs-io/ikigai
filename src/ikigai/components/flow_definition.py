b7f3bda8c5515008c8b4b3d487249d46853f2f73
Harsh Parekh <harsh@ikigailabs.io>
2025-06-03 12:45:22 -0400

    Refactor out FacetSpec from flow_definition

--- src/ikigai/components/flow_definition.py
+++ src/ikigai/components/flow_definition.py
@@ -1,61 +1,20 @@
-# SPDX-FileCopyrightText: 2024-present Harsh Parekh <harsh@ikigailabs.io>
+# SPDX-FileCopyrightText: 2025-present Harsh Parekh <harsh@ikigailabs.io>
 #
 # SPDX-License-Identifier: MIT
 
 from __future__ import annotations
 
 import logging
-from collections import ChainMap
 from random import randbytes
 from typing import Any, cast
 
-from pydantic import AliasPath, BaseModel, ConfigDict, Field, RootModel
+from pydantic import BaseModel
 
+from ikigai.components.specs import FacetSpec
 from ikigai.typing.protocol import FlowDefinitionDict
-from ikigai.typing.protocol.flow import FacetSpecsDict
 from ikigai.utils.compatibility import Self
-from ikigai.utils.custom_validators import LowercaseStr
 
 logger = logging.getLogger("ikigai.components")
-
-
-class FacetRequirementSpec(BaseModel):
-    max_child_count: int
-    min_child_count: int
-    max_parent_count: int
-    min_parent_count: int
-
-
-class ArgumentSpec(BaseModel):
-    name: str
-    argument_type: str
-    children: dict[str, ArgumentSpec]
-    have_sub_arguments: bool
-    is_deprecated: bool
-    is_hidden: bool
-    is_list: bool
-    is_required: bool
-    options: list | None = None
-
-    model_config = ConfigDict(frozen=True)
-
-
-class FacetSpec(BaseModel):
-    facet_uid: str = Field(validation_alias=AliasPath("facet_info", "facet_uid"))
-    is_deprecated: bool
-    is_hidden: bool
-    facet_requirement: FacetRequirementSpec
-    facet_arguments: list[ArgumentSpec]
-    in_arrow_arguments: list[ArgumentSpec]
-    out_arrow_arguments: list[ArgumentSpec]
-
-    model_config = ConfigDict(frozen=True)
-
-    def check_arguments(self, arguments: dict) -> None: ...
-
-    def check_in_arrows(self, arrows: list[ArrowBuilder]) -> None: ...
-
-    def check_out_arrows(self, arrows: list[ArrowBuilder]) -> None: ...
 
 
 class FacetBuilder:
@@ -67,7 +26,7 @@ class FacetBuilder:
     __builder: FlowDefinitionBuilder
 
     def __init__(
-        self, builder: FlowDefinitionBuilder, facet_type, name: str = ""
+        self, builder: FlowDefinitionBuilder, facet_type: FacetSpec, name: str = ""
     ) -> None:
         self.__builder = builder
         self.__facet_spec = facet_type
@@ -112,7 +71,6 @@ class FacetBuilder:
 
         # Check if the facet spec is satisfied
         self.__facet_spec.check_arguments(arguments=self.__arguments)
-        self.__facet_spec.check_in_arrows(arrows=self.__arrows)
 
         self.__facet = Facet(
             facet_id=randbytes(4).hex(),
@@ -148,57 +106,13 @@ class ArrowBuilder:
         )
 
 
-class FacetSpecs(BaseModel):
-    class ChainGroup(RootModel):
-        root: dict[LowercaseStr, FacetSpec]
-
-        def __post_init__(self) -> None:
-            self.root = {
-                facet_type.lower(): facet_spec
-                for facet_type, facet_spec in self.root.items()
-            }
-
-        def __contains__(self, name: str) -> bool:
-            return name.lower() in self.root
-
-        def __getitem__(self, name: str) -> FacetSpec:
-            if name not in self:
-                error_msg = f"{name.title()} facet does not exist"
-                raise AttributeError(error_msg)
-            return self.root[name.lower()]
-
-        def __getattr__(self, name: str) -> FacetSpec:
-            return self[name]
-
-        def __repr__(self) -> str:
-            keys = list(self.root.keys())
-            return f"ChainGroup({keys})"
-
-    INPUT: ChainGroup
-    MID: ChainGroup
-    OUTPUT: ChainGroup
-
-    model_config = ConfigDict(frozen=True)
-
-    @classmethod
-    def from_dict(cls, data: FacetSpecsDict) -> Self:
-        flattened_data = {
-            "INPUT": ChainMap(*data["INPUT"].values()),
-            "MID": ChainMap(*data["MID"].values()),
-            "OUTPUT": ChainMap(*data["OUTPUT"].values()),
-        }
-        self = cls.model_validate(flattened_data)
-
-        return self
-
-
 class FlowDefinitionBuilder:
     _facets: list[FacetBuilder]
 
     def __init__(self) -> None:
         self._facets = []
 
-    def facet(self, facet_type, name: str = "") -> FacetBuilder:
+    def facet(self, facet_type: FacetSpec, name: str = "") -> FacetBuilder:
         facet_builder = FacetBuilder(builder=self, facet_type=facet_type, name=name)
         self._facets.append(facet_builder)
         return facet_builder
