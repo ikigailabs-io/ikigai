5442d9e59390a843fc5cba1f552100f259a276e1
Harsh Parekh <harsh@ikigailabs.io>
2025-08-15 12:43:05 -0400

    Marked internal testing vars as internal

--- tests/components/conftest.py
+++ tests/components/conftest.py
@@ -4,25 +4,26 @@
 
 import math
 import random
-from typing import Any, Callable
+from collections.abc import Callable
+from typing import Any
 
 import pandas as pd
 import pytest
 from faker import Faker
 from ikigai import Ikigai
 
-ColumnNames = list[str]
-ColumnGenerator = Callable[[], Any]
-ColumnGenerators = list[ColumnGenerator]
-TableGenerator = tuple[ColumnNames, ColumnGenerators]
+_ColumnNames = list[str]
+_ColumnGenerator = Callable[[], Any]
+_ColumnGenerators = list[_ColumnGenerator]
+_TableGenerator = tuple[_ColumnNames, _ColumnGenerators]
 
 
-def create_table_generator(fake: Faker, k: int) -> TableGenerator:
+def _create_table_generator(fake: Faker, k: int) -> _TableGenerator:
     """
     Return a table generator that generates a table with randomly selected columns
     where, k <= number of columns <= 2k
     """
-    available_column_generators: list[list[tuple[str, ColumnGenerator]]] = [
+    available_column_generators: list[list[tuple[str, _ColumnGenerator]]] = [
         [("name", fake.name), ("age", lambda: random.randint(20, 90))],
         [("ssn", fake.ssn)],
         [
@@ -37,8 +38,8 @@ def create_table_generator(fake: Faker, k: int) -> TableGenerator:
 
     column_generators_selection = random.choices(available_column_generators, k=k)
 
-    table_column_names: ColumnNames = []
-    table_column_generators: ColumnGenerators = []
+    table_column_names: _ColumnNames = []
+    table_column_generators: _ColumnGenerators = []
     for idx, column_generators in enumerate(column_generators_selection, start=1):
         for column_name, column_generator in column_generators:
             table_column_names.append(f"{column_name}-{idx}")
@@ -56,7 +57,7 @@ def app_name(random_name: str) -> str:
     return f"proj-{random_name}"
 
 
-def generate_df(table_generator: TableGenerator, num_rows: int) -> pd.DataFrame:
+def _generate_df(table_generator: _TableGenerator, num_rows: int) -> pd.DataFrame:
     column_names, column_generators = table_generator
     rows = [[gen() for gen in column_generators] for _ in range(num_rows)]
     return pd.DataFrame(data=rows, columns=column_names)
@@ -66,16 +67,16 @@ def generate_df(table_generator: TableGenerator, num_rows: int) -> pd.DataFrame:
 def df1(faker: Faker) -> pd.DataFrame:
     num_generator_selections = random.randint(1, 10)
     num_rows = math.ceil(random.triangular(1, 100))
-    table_generator = create_table_generator(faker, num_generator_selections)
-    return generate_df(table_generator=table_generator, num_rows=num_rows)
+    table_generator = _create_table_generator(faker, num_generator_selections)
+    return _generate_df(table_generator=table_generator, num_rows=num_rows)
 
 
 @pytest.fixture
 def df2(faker: Faker) -> pd.DataFrame:
     num_generator_selections = random.randint(1, 10)
     num_rows = math.ceil(random.triangular(1, 100))
-    table_generator = create_table_generator(faker, num_generator_selections)
-    return generate_df(table_generator=table_generator, num_rows=num_rows)
+    table_generator = _create_table_generator(faker, num_generator_selections)
+    return _generate_df(table_generator=table_generator, num_rows=num_rows)
 
 
 @pytest.fixture
@@ -85,10 +86,10 @@ def df_ml_regression1(faker: Faker) -> pd.DataFrame:
     """
     num_generator_selections = random.randint(3, 10)
     num_rows = math.ceil(random.triangular(100, 1000))
-    table_generator = create_table_generator(faker, num_generator_selections)
+    table_generator = _create_table_generator(faker, num_generator_selections)
     # Add a target column for regression
     target_column_name = "target"
-    generated_df = generate_df(table_generator=table_generator, num_rows=num_rows)
+    generated_df = _generate_df(table_generator=table_generator, num_rows=num_rows)
     generated_df[target_column_name] = generated_df.apply(
         lambda _: random.uniform(0, 100), axis=1
     )
@@ -103,10 +104,10 @@ def df_ml_classification1(faker: Faker) -> pd.DataFrame:
     num_generator_selections = random.randint(3, 10)
     num_rows = math.ceil(random.triangular(100, 1000))
     num_classes = math.floor(random.triangular(2, num_rows / 3))
-    table_generator = create_table_generator(faker, num_generator_selections)
+    table_generator = _create_table_generator(faker, num_generator_selections)
     # Add a target column for classification
     target_column_name = "target"
-    generated_df = generate_df(table_generator=table_generator, num_rows=num_rows)
+    generated_df = _generate_df(table_generator=table_generator, num_rows=num_rows)
     generated_df[target_column_name] = generated_df.apply(
         lambda _: random.choice([f"class-{i}" for i in range(1, num_classes + 1)]),
         axis=1,
